9fda122f79d6d891ad9616efcbd548bdd1820c9d
Miguel Araújo <miguelbzr6@gmail.com>
2026-01-24 20:58:46 -0300

    Modify Flakes File

---
+++ flake.nix
@@ -0,0 +1,57 @@
+{
+  description = "Migg's Nix Flake ";
+
+  inputs = {
+    # Nix Packages
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    # Home Manager
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    # Hardware Otimizations
+    nixos-hardware.url = "github:nixos/nixos-hardware/master";
+  };
+
+  outputs = { self, nixpkgs, home-manager, ...}@inputs:
+  let
+    lib = nixpkgs.lib;
+      system = "x86_64-linux";
+  in{
+    nixosConfigurations = {
+      
+      # Latte Configurations
+      latte = lib.nixosSystem {
+        inherit system;
+        specialArgs = { inherit inputs; };
+        modules = [
+          ./hosts/latte/default.nix
+          home-manager.nixosModules.home-manager {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.extraSpecialArgs = { inherit inputs; };
+            home-manager.users.migg = import ./users/migg/home.nix;
+          }
+        ];
+      };
+
+      # Cappuccino Configurations
+      cappuccino = lib.nixosSystem {
+        inherit system;
+        specialArgs = { inherit inputs; };
+        modules = [
+          ./hosts/cappuccino/default.nix
+          home-manager.nixosModules.home-manager {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.extraSpecialArgs = { inherit inputs; };
+            home-manager.users.migg = import ./users/migg/home.nix;
+          }
+        ];
+      };
+
+    };
+  };
+}
