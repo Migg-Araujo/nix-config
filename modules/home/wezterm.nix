55fcd45e4ba152527e4a31c473317bf2b31e6c16
Miguel Araújo <miguelbzr6@gmail.com>
2026-03-21 12:18:38 -0300

    Add wezterm configurations

--- modules/home/wezterm.nix
+++ modules/home/wezterm.nix
@@ -1,12 +1,36 @@
 {pkgs, ...}:
 
 {
-  programs.wezterm = {
-    enable = true;
-  };
-
-  imports = [
+   imports = [
     ./zsh.nix
     ./starship.nix
+    ./fastfetch.nix
   ];
+
+  programs.wezterm = {
+    enable = true;
+    enableZshIntegration = false;
+
+    extraConfig = ''
+      local wezterm = require 'wezterm'
+      local config = {}
+
+      config.check_for_updates = false
+
+      config.window_close_confirmation = 'NeverPrompt'
+      config.color_scheme = 'Poimandres'
+      config.colors = {
+        background = "#0f0f0f"
+      }
+      config.enable_tab_bar = false
+      config.font = wezterm.font_with_fallback {
+        'JetBrainsMono Nerd Font',
+      }
+      config.font_size = 12.0
+      config.window_background_opacity = 1
+      config.audible_bell = "Disabled"
+
+      return config
+    '';
+  };
 }
--- modules/home/zsh.nix
+++ modules/home/zsh.nix
@@ -3,5 +3,25 @@
 {
   programs.zsh = {
     enable = true;
+    history = {
+      append = true;
+      share = true;
+      expireDuplicatesFirst = true;
+      ignoreDups = true;
+      size = 1000000;
+      save = 1000000;
+      path = "$HOME/.local/share/zsh/.zsh_history";
+    };
+    syntaxHighlighting.enable = true;
+    autosuggestion.enable = true;
+    initContent = ''
+      fastfetch
+      export WINIT_X11_SCALE_FACTOR=1
+      PATH=$PATH:~/.cargo/bin:~/.local/bin
+
+      bindkey "^A" vi-beginning-of-line
+      bindkey "^E" vi-end-of-line
+      bindkey '^R' fzf-history-widget
+    '';
   };
 }
